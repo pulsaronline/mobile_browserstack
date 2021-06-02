0cb224d7ab7998593c48cd7ec6338efc036a51c0
Pulsaronline <beslite@gmail.com>
2021-06-02 23:09:59 +0300

    added text Wikipedia search test for Android

--- src/test/java/tests/BrowserstackAndroidSelenideTests.java
+++ src/test/java/tests/BrowserstackAndroidSelenideTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import io.appium.java_client.MobileBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
@@ -14,7 +16,7 @@ import static io.qameta.allure.Allure.step;
 public class BrowserstackAndroidSelenideTests extends TestBase {
 
     @Test
-    @DisplayName("Successful Wikipedia Selenide test in Android app")
+    @DisplayName("Successful Wikipedia simple Selenide test in Android app")
     void androidSearchTest(){
         step("Type search", () -> {
         $(MobileBy.AccessibilityId("Search Wikipedia")).click();
@@ -23,5 +25,20 @@ public class BrowserstackAndroidSelenideTests extends TestBase {
         step("Verify content found", () -> {
         $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container")).shouldHave(sizeGreaterThan(0));
             });
+    }
+
+    @Test
+    @DisplayName("Successful Wikipedia advanced Selenide test in Android app")
+    void androidSearchTestWikipedia(){
+        step("Type search", () -> {
+            $(MobileBy.AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).val("BrowserStack");
+        });
+        step("Verify content found", () -> {
+            step("Verify search results page not empty", () ->$$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_image")).shouldHave(sizeGreaterThan(0)));
+            step("Verify Wiki container is visible", () -> $(MobileBy.id("org.wikipedia.alpha:id/search_container")).shouldBe(Condition.visible));
+            step("Verify Wiki container have header text", () -> $(MobileBy.id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(text("BrowserStack")));
+            step("Verify Wiki container have footer text", () ->$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_description")).shouldHave(text("Software company based in India")));
+        });
     }
 }
